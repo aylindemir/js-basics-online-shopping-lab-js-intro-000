95bd80ecfa7d5cbd1d8ae6dfe261af40ed3db0d5
Aylin Demir <aylin.de@outlook.com>
2020-01-06 23:06:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,10 +45,9 @@ function removeFromCart(name) {
     if(cart[i].itemName === name){
       cart.splice(i,1)
       break
-    }else if(i === cart.length-1){
-      return 'That item is not in your cart.'
     }
   }
+  return 'That item is not in your cart.'
 }
 
 
